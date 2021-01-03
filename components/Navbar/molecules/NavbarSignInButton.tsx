59002008d6a4a17df20ead0ca22204ead7142e78
Georges Saidy <georges.saidy@keeward.com>
2021-01-03 19:57:45 +0200

    Add Sign In button to navbar

---
+++ components/Navbar/molecules/NavbarSignInButton.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+import { Button } from 'antd';
+
+const NavbarSignInButton: FC = () => (
+  <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
+    <Button title="Sign In" type="primary" size="large" shape="round">
+      Sign In
+    </Button>
+  </div>
+);
+
+export default NavbarSignInButton;
