ed98f12be0810adbb574c53206bb04b5a49f11f2
Georges Saidy <georges.saidy@keeward.com>
2021-01-09 18:27:02 +0200

    Change hero structure to include atoms

--- components/Hero/molecules/HeroMain.tsx
+++ components/Hero/molecules/HeroMain.tsx
@@ -1,8 +1,8 @@
 import { FC } from 'react';
 
-import HeroTitle from './HeroTitle';
-import HeroSubtitle from './HeroSubtitle';
-import HeroButtons from './HeroButtons';
+import HeroTitle from '../atoms/HeroTitle';
+import HeroSubtitle from '../atoms/HeroSubtitle';
+import HeroButtons from '../atoms/HeroButtons';
 
 const HeroMain: FC = () => (
   <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
