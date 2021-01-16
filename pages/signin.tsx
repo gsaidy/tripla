84708168d287f97855daec27e2afecfa70852274
Georges Saidy <georges.saidy@keeward.com>
2021-01-16 22:52:37 +0200

    Add already signed in component with the logic to show it

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -1,7 +1,14 @@
 import { FC } from 'react';
+import { useSession } from 'next-auth/client';
 
 import SignIn from '../components/SignIn/SignIn';
+import AlreadySignedIn from '../components/SignIn/AlreadySignedIn';
 
-const SignInPage: FC = () => <SignIn />;
+const SignInPage: FC = () => {
+  const [session] = useSession();
+
+  if (session) return <AlreadySignedIn />;
+  return <SignIn />;
+};
 
 export default SignInPage;
