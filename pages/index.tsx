c133ab6888ba2a57d1007f2631653d545fc2dac4
Georges Saidy <georges.saidy@keeward.com>
2021-01-17 13:06:26 +0200

    Add head with title in homepage and change favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,15 @@
 import { FC } from 'react';
+import Head from 'next/head';
 
 import Hero from '../components/Hero/Hero';
 
-const Home: FC = () => <Hero />;
+const Home: FC = () => (
+  <>
+    <Head>
+      <title>Manage your trip in a simple and customizable way - Tripla</title>
+    </Head>
+    <Hero />
+  </>
+);
 
 export default Home;
