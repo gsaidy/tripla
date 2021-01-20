3145d7b26f868b4681692e4385b247d9ba91cec0
Georges Saidy <georges.saidy@keeward.com>
2021-01-21 01:45:38 +0200

    Add the list of features

--- components/Features/Features.tsx
+++ components/Features/Features.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react';
+import { LoginOutlined, FileTextOutlined, CameraOutlined, EyeOutlined } from '@ant-design/icons';
 
 import FeaturesHeader from './molecules/FeaturesHeader';
 
@@ -10,115 +11,63 @@ const Features: FC = () => (
         <dl className="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
           <div className="flex">
             <div className="flex-shrink-0">
-              <div className="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
-                <svg
-                  className="h-6 w-6"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"
-                  />
-                </svg>
+              <div className="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white text-2xl">
+                <LoginOutlined />
               </div>
             </div>
             <div className="ml-4">
               <dt className="text-lg leading-6 font-medium text-gray-900">
-                Competitive exchange rates
+                Sign in with Google, Facebook or Github
               </dt>
               <dd className="mt-2 text-base text-gray-500">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit
-                perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
+                You can still explore trips and templates and even create your own without signing
+                in. But it is recommended to sign in before creating trips and templates to be able
+                to edit them later if needed.
               </dd>
             </div>
           </div>
           <div className="flex">
             <div className="flex-shrink-0">
-              <div className="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
-                <svg
-                  className="h-6 w-6"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3"
-                  />
-                </svg>
+              <div className="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white text-2xl">
+                <FileTextOutlined />
               </div>
             </div>
             <div className="ml-4">
-              <dt className="text-lg leading-6 font-medium text-gray-900">No hidden fees</dt>
+              <dt className="text-lg leading-6 font-medium text-gray-900">Create Templates</dt>
               <dd className="mt-2 text-base text-gray-500">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit
-                perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
+                Templates are the building blocks of trips. You specify the sections and their
+                elements there. Then the template could be used in one or multiple trips.
               </dd>
             </div>
           </div>
           <div className="flex">
             <div className="flex-shrink-0">
-              <div className="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
-                <svg
-                  className="h-6 w-6"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M13 10V3L4 14h7v7l9-11h-7z"
-                  />
-                </svg>
+              <div className="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white text-2xl">
+                <CameraOutlined />
               </div>
             </div>
             <div className="ml-4">
-              <dt className="text-lg leading-6 font-medium text-gray-900">Transfers are instant</dt>
+              <dt className="text-lg leading-6 font-medium text-gray-900">Create Trips</dt>
               <dd className="mt-2 text-base text-gray-500">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit
-                perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
+                Create your trip before you leave then you will be able to access it when you reach
+                your destination so it could be your guide to make the most out of your trip.
               </dd>
             </div>
           </div>
           <div className="flex">
             <div className="flex-shrink-0">
-              <div className="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
-                <svg
-                  className="h-6 w-6"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"
-                  />
-                </svg>
+              <div className="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white text-2xl">
+                <EyeOutlined />
               </div>
             </div>
             <div className="ml-4">
-              <dt className="text-lg leading-6 font-medium text-gray-900">Mobile notifications</dt>
+              <dt className="text-lg leading-6 font-medium text-gray-900">
+                Explore other users Trips & Templates
+              </dt>
               <dd className="mt-2 text-base text-gray-500">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit
-                perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
+                Explore trips created by other users, maybe they are visiting the same destination.
+                You could also explore templates created by other users and use them to create your
+                own trip.
               </dd>
             </div>
           </div>
