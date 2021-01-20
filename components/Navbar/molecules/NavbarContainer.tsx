0d71a1c2a441a2de93d9f74de4db8a421ebd98ec
Georges Saidy <georges.saidy@keeward.com>
2021-01-20 18:13:07 +0200

    Adjust navbar max width to fit wider screens

--- components/Navbar/molecules/NavbarContainer.tsx
+++ components/Navbar/molecules/NavbarContainer.tsx
@@ -1,7 +1,7 @@
 import { FC, ReactNode } from 'react';
 
 const NavbarContainer: FC<{ children: ReactNode }> = ({ children }) => (
-  <div className="max-w-7xl mx-auto px-4 sm:px-6">
+  <div className="max-w-8xl mx-auto px-4 sm:px-6">
     <div className="flex justify-between items-center border-b-2 border-gray-100 pt-4 pb-2 md:justify-start md:space-x-10">
       {children}
     </div>
