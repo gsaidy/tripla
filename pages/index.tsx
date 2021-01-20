00f2a5598068a42a025e06ca741295d9d0d13c4e
Georges Saidy <georges.saidy@keeward.com>
2021-01-21 00:21:55 +0200

    Add placeholder feature section in homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,17 @@ import { FC } from 'react';
 import Head from 'next/head';
 
 import Hero from '../components/Hero/Hero';
+import Features from '../components/Features/Features';
 
 const Home: FC = () => (
   <>
     <Head>
       <title>Manage your trip in a simple and customizable way - Tripla</title>
     </Head>
-    <Hero />
+    <div className="min-h-tripla">
+      <Hero />
+      <Features />
+    </div>
   </>
 );
 
