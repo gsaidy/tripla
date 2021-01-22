81cc0a234afd1dc6158db9f51ed79a163233ae80
Georges Saidy <georges.saidy@keeward.com>
2021-01-23 00:29:45 +0200

    Show sign in banner only if user is not logged in

--- components/Templates/CreateTemplate.tsx
+++ components/Templates/CreateTemplate.tsx
@@ -1,7 +1,15 @@
 import { FC } from 'react';
+import { useSession } from 'next-auth/client';
 
 import SignInBanner from '../SignIn/molecules/SignInBanner';
 
-const CreateTemplate: FC = () => <SignInBanner />;
+const CreateTemplate: FC = () => {
+  const [session, loading] = useSession();
+
+  if (!session && !loading) {
+    return <SignInBanner />;
+  }
+  return null;
+};
 
 export default CreateTemplate;
