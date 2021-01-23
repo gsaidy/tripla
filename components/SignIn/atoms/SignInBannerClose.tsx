5ccbc982098be5aacb3220226909310108565b2d
Georges Saidy <georges.saidy@keeward.com>
2021-01-23 02:11:04 +0200

    Hide banner on close icon click

--- components/SignIn/atoms/SignInBannerClose.tsx
+++ components/SignIn/atoms/SignInBannerClose.tsx
@@ -1,11 +1,11 @@
 import { FC } from 'react';
 import { CloseOutlined } from '@ant-design/icons';
 
-const SignInBannerClose: FC = () => (
+const SignInBannerClose: FC<{ onClick: () => void }> = ({ onClick }) => (
   <div className="order-2 flex-shrink-0 sm:order-3 sm:ml-3 text-white text-xl">
     <button
-      type="button"
       className="-mr-1 flex p-2 rounded-md hover:bg-blue-800 focus:outline-none sm:-mr-2"
+      onClick={onClick}
     >
       <span className="sr-only">Dismiss</span>
       <CloseOutlined />
--- components/SignIn/molecules/SignInBanner.tsx
+++ components/SignIn/molecules/SignInBanner.tsx
@@ -1,21 +1,24 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 
 import SignInBannerButton from '../atoms/SignInBannerButton';
 import SignInBannerText from '../atoms/SignInBannerText';
 import SignInBannerClose from '../atoms/SignInBannerClose';
 
-const SignInBanner: FC = () => (
-  <div className="bg-blue-300">
-    <div className="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
+const SignInBanner: FC = () => {
+  const [hideBanner, setHideBanner] = useState(false);
+
+  if (hideBanner) return null;
+  return (
+    <div className="bg-blue-300 max-w-8xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
       <div className="flex items-center justify-between flex-wrap">
         <div className="w-0 flex-1 flex items-center">
           <SignInBannerButton />
           <SignInBannerText />
         </div>
-        <SignInBannerClose />
+        <SignInBannerClose onClick={() => setHideBanner(true)} />
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default SignInBanner;
