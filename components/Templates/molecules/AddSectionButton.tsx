ff22347ce36f50d1967c74cb3996a530af6b727a
Georges Saidy <georges.saidy@keeward.com>
2021-01-28 21:45:17 +0200

    Append Add Section button in template form

---
+++ components/Templates/molecules/AddSectionButton.tsx
@@ -0,0 +1,23 @@
+import { FC } from 'react';
+import { Button } from 'antd';
+import { PlusOutlined } from '@ant-design/icons';
+
+const AddSectionButton: FC = () => (
+  <div className="max-w-7xl mx-auto py-4 sm:px-6 lg:px-8">
+    <div className="md:grid md:grid-cols-6">
+      <div className="md:col-start-2 md:col-span-5">
+        <Button
+          className="flex justify-center items-center"
+          type="dashed"
+          size="large"
+          block
+          icon={<PlusOutlined />}
+        >
+          Add Section
+        </Button>
+      </div>
+    </div>
+  </div>
+);
+
+export default AddSectionButton;
--- components/Templates/organisms/TemplateForm.tsx
+++ components/Templates/organisms/TemplateForm.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react';
 import { Form } from 'antd';
 
 import BasicInfo from '../molecules/BasicInfo';
+import AddSectionButton from '../molecules/AddSectionButton';
 
 const TemplateForm: FC = () => {
   const layout = {
@@ -11,6 +12,7 @@ const TemplateForm: FC = () => {
   return (
     <Form {...layout}>
       <BasicInfo />
+      <AddSectionButton />
     </Form>
   );
 };
