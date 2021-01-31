35ef492ab59efe2b103d43f0ffe104d600d6449c
Georges Saidy <georges.saidy@keeward.com>
2021-01-31 14:52:08 +0200

    Add EntityHeader and use it for section and attribute headers

--- components/SectionAttributes/atoms/AttributeHeader.tsx
+++ components/SectionAttributes/atoms/AttributeHeader.tsx
@@ -1,14 +1,10 @@
 import { FC } from 'react';
-import { MinusCircleOutlined } from '@ant-design/icons';
+
+import EntityHeader from '../../utilities/EntityHeader';
 
 const AttributeHeader: FC<{ title: string; removeAttribute: () => void }> = ({
   title,
   removeAttribute,
-}) => (
-  <div className="flex items-center border-b pb-1">
-    <h3 className="font-bold text-gray-900">{title}</h3>
-    <MinusCircleOutlined className="ml-1 cursor-pointer text-red-500" onClick={removeAttribute} />
-  </div>
-);
+}) => <EntityHeader className="border-b pb-1" title={title} remove={removeAttribute} />;
 
 export default AttributeHeader;
--- components/Templates/atoms/SectionHeader.tsx
+++ components/Templates/atoms/SectionHeader.tsx
@@ -1,14 +1,10 @@
 import { FC } from 'react';
-import { MinusCircleOutlined } from '@ant-design/icons';
+
+import EntityHeader from '../../utilities/EntityHeader';
 
 const SectionHeader: FC<{ title: string; removeSection: () => void }> = ({
   title,
   removeSection,
-}) => (
-  <div className="flex items-center">
-    <h3>{title}</h3>
-    <MinusCircleOutlined className="ml-1 cursor-pointer text-red-500" onClick={removeSection} />
-  </div>
-);
+}) => <EntityHeader title={title} remove={removeSection} />;
 
 export default SectionHeader;
