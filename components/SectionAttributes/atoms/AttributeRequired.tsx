dfac05c97c2f54b86f314c787f5123cfa3aad028
Georges Saidy <georges.saidy@keeward.com>
2021-02-06 17:15:42 +0200

    Add name attribute to attribute required

--- components/SectionAttributes/atoms/AttributeRequired.tsx
+++ components/SectionAttributes/atoms/AttributeRequired.tsx
@@ -1,9 +1,9 @@
 import { FC } from 'react';
 import { Form, Switch } from 'antd';
 
-const AttributeRequired: FC = () => (
+const AttributeRequired: FC<{ parentName: number }> = ({ parentName }) => (
   <div className="col-span-6 sm:col-span-4 lg:col-span-3 -mt-3 -mb-6 xs:my-0">
-    <Form.Item label="Required">
+    <Form.Item label="Required" name={[parentName, 'attributeRequired']} valuePropName="checked">
       <Switch className="-mt-5 xs:mt-0" />
     </Form.Item>
   </div>
--- components/SectionAttributes/molecules/SectionAttribute.tsx
+++ components/SectionAttributes/molecules/SectionAttribute.tsx
@@ -20,7 +20,7 @@ const SectionAttribute: FC<{ index: number; name: number; removeAttribute: () =>
       <AttributeHeader title={`Attribute ${index + 1}`} removeAttribute={removeAttribute} />
       <div className="grid grid-cols-6 gap-x-2 mt-2 xs:mt-6 mb-1">
         <AttributeName parentName={name} />
-        <AttributeRequired />
+        <AttributeRequired parentName={name} />
         <AttributeEdit setValue={setEditValue} />
         <AttributeView setValue={setViewValue} />
       </div>
