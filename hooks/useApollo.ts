5c9bd76f00b1232eb76b622354da8d4a6dc460ac
Georges Saidy <georges.saidy@keeward.com>
2021-02-13 00:01:11 +0200

    Add useApollo hook that will be used to connect to GraphQL API

---
+++ hooks/useApollo.ts
@@ -0,0 +1,20 @@
+import { InMemoryCache, ApolloClient, HttpLink, NormalizedCacheObject } from '@apollo/client';
+
+let apolloClient: ApolloClient<NormalizedCacheObject>;
+
+const createApolloClient = () => {
+  return new ApolloClient({
+    link: new HttpLink({
+      uri: process.env.NEXT_PUBLIC_HASURA_GRAPHQL_API,
+      headers: {
+        'content-type': 'application/json',
+        'x-hasura-admin-secret': process.env.NEXT_PUBLIC_HASURA_ADMIN_SECRET,
+      },
+    }),
+    cache: new InMemoryCache(),
+  });
+};
+
+const useApollo = (): ApolloClient<NormalizedCacheObject> => apolloClient ?? createApolloClient();
+
+export default useApollo;
