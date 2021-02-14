2f50fa3f06107a47b090bb2239c6de99c1e2c892
Georges Saidy <georges.saidy@keeward.com>
2021-02-14 03:28:24 +0200

    Extract template file under interfaces into multiple files

--- interfaces/template.ts
+++ interfaces/template.ts
@@ -1,29 +1,9 @@
-import EditType from 'enums/editType';
-import ViewType from 'enums/viewType';
+import Section from './section';
 
 interface Template {
   name: string;
   description?: string;
   sections?: Section[];
-}
-
-interface Section {
-  name: string;
-  order?: number;
-  attributes?: Attribute[];
-}
-
-interface Attribute {
-  name: string;
-  required?: boolean;
-  edit: EditType;
-  view: ViewType;
-  options?: Option[];
-}
-
-interface Option {
-  name: string;
-  color?: string;
 }
 
 export default Template;
