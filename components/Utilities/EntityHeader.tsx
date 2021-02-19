c3372ac8f1684fe6fba639e20a66503ceb305a59
Georges Saidy <georges.saidy@keeward.com>
2021-02-19 15:52:31 +0200

    Hide remove entity icon in view mode

--- components/Utilities/EntityHeader.tsx
+++ components/Utilities/EntityHeader.tsx
@@ -1,15 +1,24 @@
-import { FC } from 'react';
+import { FC, useContext } from 'react';
 import { MinusCircleOutlined } from '@ant-design/icons';
+
+import { TemplateFormContext } from '../Templates/organisms/TemplateForm';
+import FormMode from 'enums/formMode';
 
 const EntityHeader: FC<{ className?: string; title: string; remove: () => void }> = ({
   className,
   title,
   remove,
-}) => (
-  <div className={`flex items-center ${className}`}>
-    <h3>{title}</h3>
-    <MinusCircleOutlined className="ml-1 cursor-pointer text-red-500" onClick={remove} />
-  </div>
-);
+}) => {
+  const formMode = useContext(TemplateFormContext);
+
+  return (
+    <div className={`flex items-center ${className}`}>
+      <h3>{title}</h3>
+      {formMode !== FormMode.View && (
+        <MinusCircleOutlined className="ml-1 cursor-pointer text-red-500" onClick={remove} />
+      )}
+    </div>
+  );
+};
 
 export default EntityHeader;
