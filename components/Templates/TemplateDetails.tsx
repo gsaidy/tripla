dc01b46441ff08a6cd95976e8d890841ab82590d
Georges Saidy <georges.saidy@keeward.com>
2021-02-19 03:04:55 +0200

    Show template not found when query returns no result

--- components/Templates/TemplateDetails.tsx
+++ components/Templates/TemplateDetails.tsx
@@ -4,14 +4,28 @@ import { useQuery } from '@apollo/client';
 import GET_TEMPLATE_DETAILS from 'gql/queries/getTemplateDetails';
 import PageLoader from '../PageLoader/PageLoader';
 import ErrorResult from '../ErrorResult/ErrorResult';
+import NotFound from '../NotFound/NotFound';
 
 const TemplateDetails: FC<{ id: string | string[] }> = ({ id }) => {
-  const { loading, error } = useQuery(GET_TEMPLATE_DETAILS, { variables: { id } });
+  const { loading, error, data } = useQuery(GET_TEMPLATE_DETAILS, { variables: { id } });
   if (loading) {
     return <PageLoader />;
   }
   if (error) {
     return <ErrorResult />;
+  }
+  const {
+    templates: [template],
+  } = data;
+  if (!template) {
+    return (
+      <NotFound
+        title="Template Not Found"
+        subTitle="Sorry, the requested template does not exist."
+        href="/templates"
+        buttonText="Back to Template List"
+      />
+    );
   }
   return null;
 };
