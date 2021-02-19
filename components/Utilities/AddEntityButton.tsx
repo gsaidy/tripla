593edfefdef3eff1ba8423e14ca6d4e7db9dcea9
Georges Saidy <georges.saidy@keeward.com>
2021-02-19 15:58:53 +0200

    Do not show add entity button in view mode

--- components/Utilities/AddEntityButton.tsx
+++ components/Utilities/AddEntityButton.tsx
@@ -1,18 +1,29 @@
-import { FC } from 'react';
+import { FC, useContext } from 'react';
 import { Button } from 'antd';
 import { PlusOutlined } from '@ant-design/icons';
 
-const AddEntityButton: FC<{ entity: string; add: () => void }> = ({ entity, add }) => (
-  <Button
-    className="flex justify-center items-center"
-    type="dashed"
-    size="large"
-    block
-    icon={<PlusOutlined />}
-    onClick={add}
-  >
-    Add {entity}
-  </Button>
-);
+import FormMode from 'enums/formMode';
+import { TemplateFormContext } from '../Templates/organisms/TemplateForm';
+
+const AddEntityButton: FC<{ entity: string; add: () => void }> = ({ entity, add }) => {
+  const formMode = useContext(TemplateFormContext);
+
+  if (formMode === FormMode.View) {
+    return null;
+  }
+
+  return (
+    <Button
+      className="flex justify-center items-center"
+      type="dashed"
+      size="large"
+      block
+      icon={<PlusOutlined />}
+      onClick={add}
+    >
+      Add {entity}
+    </Button>
+  );
+};
 
 export default AddEntityButton;
