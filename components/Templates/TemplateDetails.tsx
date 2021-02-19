1ec3c3e9ff0b314d59dee3fa9b2a25ab1e17224d
Georges Saidy <georges.saidy@keeward.com>
2021-02-19 02:50:52 +0200

    Show error result when query returns an error

--- components/Templates/TemplateDetails.tsx
+++ components/Templates/TemplateDetails.tsx
@@ -3,11 +3,15 @@ import { useQuery } from '@apollo/client';
 
 import GET_TEMPLATE_DETAILS from 'gql/queries/getTemplateDetails';
 import PageLoader from '../PageLoader/PageLoader';
+import ErrorResult from '../ErrorResult/ErrorResult';
 
 const TemplateDetails: FC<{ id: string | string[] }> = ({ id }) => {
-  const { loading } = useQuery(GET_TEMPLATE_DETAILS, { variables: { id } });
+  const { loading, error } = useQuery(GET_TEMPLATE_DETAILS, { variables: { id } });
   if (loading) {
     return <PageLoader />;
+  }
+  if (error) {
+    return <ErrorResult />;
   }
   return null;
 };
