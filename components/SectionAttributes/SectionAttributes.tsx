989a91518dc67b6057e760a1c3691c475809ab13
Georges Saidy <georges.saidy@keeward.com>
2021-02-20 17:59:18 +0200

    Show attributes and options as collapsed by default in view mode

--- components/SectionAttributes/SectionAttributes.tsx
+++ components/SectionAttributes/SectionAttributes.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useContext } from 'react';
 import { Collapse, Form } from 'antd';
 const { Panel } = Collapse;
 
@@ -7,33 +7,39 @@ import SectionAttribute from './molecules/SectionAttribute';
 import AddAttributeButton from './molecules/AddAttributeButton';
 import EditType from 'enums/editType';
 import ViewType from 'enums/viewType';
+import { TemplateFormContext } from '../Templates/organisms/TemplateForm';
+import FormMode from 'enums/formMode';
 
-const SectionAttributes: FC<{ parentName: number }> = ({ parentName }) => (
-  <Collapse defaultActiveKey="1">
-    <Panel
-      key="1"
-      header="Attributes"
-      extra={<EntityInfoTooltip title="A section must have at least one attribute" />}
-    >
-      <Form.List name={[parentName, 'attributes']}>
-        {(fields, { add, remove }) => (
-          <>
-            {fields.map((field, index) => (
-              <SectionAttribute
-                key={field.key}
-                index={index}
-                name={field.name}
-                removeAttribute={() => remove(field.name)}
+const SectionAttributes: FC<{ parentName: number }> = ({ parentName }) => {
+  const formMode = useContext(TemplateFormContext);
+
+  return (
+    <Collapse defaultActiveKey={formMode === FormMode.Create ? '1' : '0'}>
+      <Panel
+        key="1"
+        header="Attributes"
+        extra={<EntityInfoTooltip title="A section must have at least one attribute" />}
+      >
+        <Form.List name={[parentName, 'attributes']}>
+          {(fields, { add, remove }) => (
+            <>
+              {fields.map((field, index) => (
+                <SectionAttribute
+                  key={field.key}
+                  index={index}
+                  name={field.name}
+                  removeAttribute={() => remove(field.name)}
+                />
+              ))}
+              <AddAttributeButton
+                addAttribute={() => add({ edit: EditType.Input, view: ViewType.Label })}
               />
-            ))}
-            <AddAttributeButton
-              addAttribute={() => add({ edit: EditType.Input, view: ViewType.Label })}
-            />
-          </>
-        )}
-      </Form.List>
-    </Panel>
-  </Collapse>
-);
+            </>
+          )}
+        </Form.List>
+      </Panel>
+    </Collapse>
+  );
+};
 
 export default SectionAttributes;
