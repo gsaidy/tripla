5634bd9139dc857214e393c6d0f985ca1b1652d8
Georges Saidy <georges.saidy@keeward.com>
2021-02-20 19:14:36 +0200

    Do not show entity info tooltip in view mode

--- components/Utilities/EntityInfoTooltip.tsx
+++ components/Utilities/EntityInfoTooltip.tsx
@@ -1,11 +1,21 @@
-import { FC } from 'react';
+import { FC, useContext } from 'react';
 import { Tooltip } from 'antd';
 import { InfoCircleOutlined } from '@ant-design/icons';
 
-const EntityInfoTooltip: FC<{ title: string }> = ({ title }) => (
-  <Tooltip placement="topRight" title={title}>
-    <InfoCircleOutlined />
-  </Tooltip>
-);
+import { TemplateFormContext } from '../Templates/organisms/TemplateForm';
+import FormMode from 'enums/formMode';
+
+const EntityInfoTooltip: FC<{ title: string }> = ({ title }) => {
+  const formMode = useContext(TemplateFormContext);
+
+  if (formMode !== FormMode.View) {
+    return (
+      <Tooltip placement="topRight" title={title}>
+        <InfoCircleOutlined />
+      </Tooltip>
+    );
+  }
+  return null;
+};
 
 export default EntityInfoTooltip;
