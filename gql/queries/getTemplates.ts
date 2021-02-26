975281a3e6dd21a7762e95027799b23d45bac19b
Georges Saidy <georges.saidy@keeward.com>
2021-02-26 23:35:06 +0200

    Add get templates query

---
+++ gql/queries/getTemplates.ts
@@ -0,0 +1,23 @@
+import { gql } from '@apollo/client';
+
+const GET_TEMPLATES = gql`
+  query GetTemplates(
+    $offset: Int!
+    $limit: Int!
+    $where: templates_bool_exp
+    $order_by: [templates_order_by!]
+  ) {
+    templates(offset: $offset, limit: $limit, where: $where, order_by: $order_by) {
+      id
+      name
+      description
+      user {
+        name
+      }
+      created_at
+      updated_at
+    }
+  }
+`;
+
+export default GET_TEMPLATES;
