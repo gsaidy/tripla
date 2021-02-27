e50dc88b2cd3ca82b106e50dc214b46fba3971fd
Georges Saidy <georges.saidy@keeward.com>
2021-02-27 15:36:57 +0200

    Unify template list components into one

--- components/Templates/Templates.tsx
+++ components/Templates/Templates.tsx
@@ -2,9 +2,10 @@ import { FC } from 'react';
 import { useSession } from 'next-auth/client';
 
 import PageLoader from '../PageLoader/PageLoader';
-import MyTemplateList from './organisms/MyTemplateList';
-import OtherUsersTemplateList from './organisms/OtherUsersTemplateList';
-import TemplateList from './organisms/TemplateList';
+import MyTemplateList from './organisms/TemplateList';
+import OtherUsersTemplateList from './organisms/TemplateList';
+import AllTemplateList from './organisms/TemplateList';
+import CreatorFilter from 'enums/creatorFilter';
 
 const Templates: FC = () => {
   const [session, loading] = useSession();
@@ -16,11 +17,15 @@ const Templates: FC = () => {
     <div className="min-h-tripla">
       {session ? (
         <>
-          <MyTemplateList />
-          <OtherUsersTemplateList />
+          <MyTemplateList createdBy={CreatorFilter.Me} title="My Templates" className="pt-6" />
+          <OtherUsersTemplateList
+            createdBy={CreatorFilter.Other}
+            title="Other Users Templates"
+            className="-mt-6"
+          />
         </>
       ) : (
-        <TemplateList />
+        <AllTemplateList createdBy={CreatorFilter.All} title="Templates" className="pt-6" />
       )}
     </div>
   );
--- components/Templates/organisms/TemplateList.tsx
+++ components/Templates/organisms/TemplateList.tsx
@@ -1,14 +1,34 @@
 import { FC } from 'react';
 import { useQuery } from '@apollo/client';
+import { useSession } from 'next-auth/client';
 
 import TemplateTable from '../molecules/TemplateTable';
 import GET_TEMPLATES from 'gql/queries/getTemplates';
 import PageLoader from '../../PageLoader/PageLoader';
 import ErrorResult from '../../ErrorResult/ErrorResult';
+import CreatorFilter from 'enums/creatorFilter';
+import User from 'interfaces/user';
 
-const TemplateList: FC = () => {
+const TemplateList: FC<{ createdBy: CreatorFilter; title: string; className: string }> = ({
+  createdBy,
+  title,
+  className,
+}) => {
+  const [session] = useSession();
+  const user = session?.user as User;
+
+  const getUserFilter = () => {
+    if (createdBy === CreatorFilter.Me) {
+      return { userId: { _eq: user.id } };
+    }
+    if (createdBy === CreatorFilter.Other) {
+      return { userId: { _neq: user.id } };
+    }
+    return;
+  };
+
   const { loading, error, data } = useQuery(GET_TEMPLATES, {
-    variables: { offset: 0, limit: 10 },
+    variables: { offset: 0, limit: 10, where: getUserFilter() },
   });
 
   if (loading) {
@@ -19,8 +39,8 @@ const TemplateList: FC = () => {
   }
   const { templates } = data;
   return (
-    <div className="pt-4">
-      <TemplateTable title="Templates" data={templates} />
+    <div className={className}>
+      <TemplateTable title={title} data={templates} />
     </div>
   );
 };
