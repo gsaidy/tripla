b86b2224d6d3d3ad41b405b9cea547e5b079ae13
Georges Saidy <georges.saidy@keeward.com>
2021-03-03 23:13:20 +0200

    Move the table action to the constant file

--- components/Templates/molecules/TemplateTable.tsx
+++ components/Templates/molecules/TemplateTable.tsx
@@ -1,12 +1,11 @@
 import { FC } from 'react';
 import { Table } from 'antd';
-import { ColumnsType, TablePaginationConfig } from 'antd/lib/table';
+import { TablePaginationConfig } from 'antd/lib/table';
 
 import templateTableColumns from 'constants/templateTableColumns';
 import TemplateOverview from 'interfaces/templateOverview';
 import TemplateTableHeader from '../atoms/TemplateTableHeader';
 import CreatorFilter from 'enums/creatorFilter';
-import TemplateTableViewButton from '../atoms/TemplateTableViewButton';
 
 const TemplateTable: FC<{
   createdBy: CreatorFilter;
@@ -15,36 +14,24 @@ const TemplateTable: FC<{
   pagination: TablePaginationConfig | Record<string, never>;
   loading: boolean;
   onChange: (pagination: TablePaginationConfig) => void;
-}> = ({ createdBy, title, data, pagination, loading, onChange }) => {
-  const action = {
-    title: 'Action',
-    key: 'action',
-    width: '9%',
-    fixed: 'right',
-    render(_: unknown, { id }: TemplateOverview) {
-      return <TemplateTableViewButton id={id} />;
-    },
-  };
-
-  return (
-    <div className="max-w-7xl mx-auto py-6 px-4 lg:px-6">
-      <TemplateTableHeader title={title} showCreateButton={createdBy !== CreatorFilter.Other} />
-      <Table
-        rowKey="id"
-        columns={[...templateTableColumns, action] as ColumnsType<TemplateOverview>}
-        dataSource={data}
-        pagination={{
-          ...pagination,
-          showTotal: (total: number, range: number[]) =>
-            `${range[0]}-${range[1]} of ${total} templates`,
-        }}
-        locale={{ emptyText: 'No Templates' }}
-        scroll={{ x: 1000 }}
-        loading={loading}
-        onChange={onChange}
-      />
-    </div>
-  );
-};
+}> = ({ createdBy, title, data, pagination, loading, onChange }) => (
+  <div className="max-w-7xl mx-auto py-6 px-4 lg:px-6">
+    <TemplateTableHeader title={title} showCreateButton={createdBy !== CreatorFilter.Other} />
+    <Table
+      rowKey="id"
+      columns={templateTableColumns}
+      dataSource={data}
+      pagination={{
+        ...pagination,
+        showTotal: (total: number, range: number[]) =>
+          `${range[0]}-${range[1]} of ${total} templates`,
+      }}
+      locale={{ emptyText: 'No Templates' }}
+      scroll={{ x: 1000 }}
+      loading={loading}
+      onChange={onChange}
+    />
+  </div>
+);
 
 export default TemplateTable;
