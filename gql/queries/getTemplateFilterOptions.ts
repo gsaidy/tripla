cc6fca6b81d47e1e323fd88e9467a4a3175a0e6f
Georges Saidy <georges.saidy@keeward.com>
2021-03-06 01:52:58 +0200

    Add ability to filter templates by name

---
+++ gql/queries/getTemplateFilterOptions.ts
@@ -0,0 +1,11 @@
+import { gql } from '@apollo/client';
+
+const GET_TEMPLATE_FILTER_OPTIONS = gql`
+  query GetTemplates($where: templates_bool_exp) {
+    templates(where: $where, order_by: { name: asc }) {
+      name
+    }
+  }
+`;
+
+export default GET_TEMPLATE_FILTER_OPTIONS;
---
+++ utils/filters.ts
@@ -0,0 +1,36 @@
+import CreatorFilter from 'enums/creatorFilter';
+import User from 'interfaces/user';
+
+export const getTemplateFilters = (
+  createdBy: CreatorFilter,
+  user: User,
+  name?: string
+): unknown => {
+  if (name) {
+    return withNameFilter(createdBy, user, name);
+  }
+  return withoutNameFilter(createdBy, user);
+};
+
+const withNameFilter = (createdBy: CreatorFilter, user: User, name: string) => {
+  if (createdBy === CreatorFilter.Me) {
+    return { _and: [{ userId: { _eq: `${user.id}` } }, { name: { _eq: name } }] };
+  }
+  if (createdBy === CreatorFilter.Other) {
+    return {
+      _or: [{ userId: { _neq: `${user.id}` } }, { userId: { _is_null: true } }],
+      _and: { name: { _eq: name } },
+    };
+  }
+  return { name: { _eq: name } };
+};
+
+const withoutNameFilter = (createdBy: CreatorFilter, user: User) => {
+  if (createdBy === CreatorFilter.Me) {
+    return { userId: { _eq: `${user.id}` } };
+  }
+  if (createdBy === CreatorFilter.Other) {
+    return { _or: [{ userId: { _neq: `${user.id}` } }, { userId: { _is_null: true } }] };
+  }
+  return;
+};
