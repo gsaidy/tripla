031bac5798a537e1355122258664b7b2faa13904
Georges Saidy <georges.saidy@keeward.com>
2021-03-13 18:32:09 +0200

    Add blank trip details page

---
+++ pages/trips/[slug].tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react';
+import Head from 'next/head';
+
+const TripDetailsPage: FC = () => (
+  <>
+    <Head>
+      <title>Trip Details - Tripla</title>
+    </Head>
+    <h1>Under construction</h1>
+  </>
+);
+
+export default TripDetailsPage;
