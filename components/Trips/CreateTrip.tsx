cc34e61809328782d380caabd5c8b44518a96554
Georges Saidy <georges.saidy@keeward.com>
2021-03-13 15:24:13 +0200

    Add create trip button in create page

--- components/Trips/CreateTrip.tsx
+++ components/Trips/CreateTrip.tsx
@@ -4,6 +4,8 @@ import { useSession } from 'next-auth/client';
 import SignInBanner from '../SignIn/molecules/SignInBanner';
 import EntityType from 'enums/entityType';
 import TripForm from './organisms/TripForm';
+import FormActions from '../FormActions/FormActions';
+import SubmitButton from '../FormActions/atoms/SubmitButton';
 
 const CreateTrip: FC = () => {
   const [session, loading] = useSession();
@@ -11,7 +13,11 @@ const CreateTrip: FC = () => {
   return (
     <div className="min-h-tripla bg-gray-50">
       {!session && !loading && <SignInBanner entity={EntityType.Trip} />}
-      <TripForm />
+      <TripForm>
+        <FormActions>
+          <SubmitButton label="Create Trip" loading={false} />
+        </FormActions>
+      </TripForm>
     </div>
   );
 };
--- components/Trips/organisms/TripForm.tsx
+++ components/Trips/organisms/TripForm.tsx
@@ -1,11 +1,12 @@
-import { FC } from 'react';
+import { FC, ReactNode } from 'react';
 import { Form } from 'antd';
 
 import BasicInfo from '../molecules/BasicInfo';
 
-const TripForm: FC = () => (
-  <Form labelCol={{ span: 6 }}>
+const TripForm: FC<{ children?: ReactNode }> = ({ children }) => (
+  <Form className="space-y-4" labelCol={{ span: 6 }}>
     <BasicInfo />
+    {children}
   </Form>
 );
 
