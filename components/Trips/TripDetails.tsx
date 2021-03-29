962dc4299cae0d61ebd617db3ac71480d4f29b7b
Georges Saidy <georges.saidy@keeward.com>
2021-03-29 20:11:54 +0300

    Display edit and delete trip buttons in view mode

--- components/Trips/TripDetails.tsx
+++ components/Trips/TripDetails.tsx
@@ -1,5 +1,6 @@
 import { FC, useState } from 'react';
 import { useQuery } from '@apollo/client';
+import { useSession } from 'next-auth/client';
 
 import GET_TRIP_DETAILS from 'gql/queries/getTripDetails';
 import PageLoader from '../PageLoader/PageLoader';
@@ -8,9 +9,16 @@ import NotFound from '../NotFound/NotFound';
 import FormMode from 'enums/formMode';
 import Trip from 'interfaces/trip';
 import TripForm from './organisms/TripForm';
+import User from 'interfaces/user';
+import { sameUser } from 'utils/user';
+import FormActions from '../FormActions/FormActions';
+import EditButton from '../FormActions/atoms/EditButton';
+import EntityType from 'enums/entityType';
+import DeleteButton from '../FormActions/atoms/DeleteButton';
 
 const TripDetails: FC<{ id: string | string[] }> = ({ id }) => {
-  const [formMode] = useState(FormMode.View);
+  const [session] = useSession();
+  const [formMode, setFormMode] = useState(FormMode.View);
   const { loading, error, data } = useQuery(GET_TRIP_DETAILS, { variables: { id } });
 
   if (loading) {
@@ -31,6 +39,12 @@ const TripDetails: FC<{ id: string | string[] }> = ({ id }) => {
     );
   }
 
+  const userHasPermissionToEditOrDelete = () => {
+    const loggedInUser = session?.user as User;
+    const { user: tripCreatedByUser } = trip;
+    return sameUser(loggedInUser, tripCreatedByUser);
+  };
+
   const updateTrip = (trip: Trip) => {
     console.log(trip);
   };
@@ -41,7 +55,18 @@ const TripDetails: FC<{ id: string | string[] }> = ({ id }) => {
         formMode={formMode}
         tripInitialData={{ ...trip, templateId: trip.template.id }}
         onSubmit={updateTrip}
-      />
+      >
+        {userHasPermissionToEditOrDelete() && (
+          <FormActions className={formMode === FormMode.View ? '-mt-3' : ''}>
+            {formMode === FormMode.View && (
+              <>
+                <EditButton entity={EntityType.Trip} onClick={() => setFormMode(FormMode.Edit)} />
+                <DeleteButton entity={EntityType.Trip} onConfirm={() => ({})} />
+              </>
+            )}
+          </FormActions>
+        )}
+      </TripForm>
     </div>
   );
 };
