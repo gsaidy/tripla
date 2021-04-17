d0cfe0f5027885bef8903512e37eb9b0805c86df
Georges Saidy <georges.saidy@keeward.com>
2021-04-17 21:00:22 +0300

    Move section table up

--- components/TripSections/molecules/TripSection.tsx
+++ components/TripSections/molecules/TripSection.tsx
@@ -1,14 +1,24 @@
 import { FC } from 'react';
+import { Table } from 'antd';
 
 import Section from 'interfaces/section';
 import TripSectionHeader from '../atoms/TripSectionHeader';
-import TripSectionTable from '../atoms/TripSectionTable';
+import Attribute from 'interfaces/attribute';
 
-const TripSection: FC<{ section: Section }> = ({ section }) => (
-  <div>
-    <TripSectionHeader name={section.name} />
-    <TripSectionTable name={section.name} attributes={section.attributes} />
-  </div>
-);
+const TripSection: FC<{ section: Section }> = ({ section }) => {
+  const columns = section.attributes.map(({ name }: Attribute) => ({
+    title: name,
+    dataIndex: name,
+    ellipsis: true,
+    align: 'center' as 'center' | 'left' | 'right',
+  }));
+
+  return (
+    <div>
+      <TripSectionHeader name={section.name} />
+      <Table columns={columns} locale={{ emptyText: `No ${section.name} added yet.` }} />
+    </div>
+  );
+};
 
 export default TripSection;
