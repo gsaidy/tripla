667fc06a82d281c3150d33adff7cf282c46f17eb
Georges Saidy <georges.saidy@keeward.com>
2021-04-24 00:07:01 +0300

    Remove custom field component and render inputs at higher level

--- components/TripSectionModal/molecules/TripSectionModalField.tsx
+++ components/TripSectionModal/molecules/TripSectionModalField.tsx
@@ -1,19 +1,51 @@
 import { FC } from 'react';
-import { Form } from 'antd';
+import { Form, Input, Select, DatePicker, TimePicker } from 'antd';
+const { Option: SelectOption } = Select;
+const { RangePicker: DateRangePicker } = DatePicker;
+const { RangePicker: TimeRangePicker } = TimePicker;
+const { TextArea } = Input;
 
-import CustomField from '../atoms/CustomField';
 import EditType from 'enums/editType';
 import Option from 'interfaces/option';
+import { TIME_FORMAT } from 'constants/dateTimeFormats';
 
 const TripSectionModalField: FC<{
   name: string;
   required?: boolean;
   type: EditType;
   options?: Option[];
-}> = ({ name, required, type, options = [] }) => (
-  <Form.Item name={name} label={name} rules={[{ required, message: `Please select a ${name}.` }]}>
-    <CustomField type={type} options={options} />
-  </Form.Item>
-);
+}> = ({ name, required, type, options = [] }) => {
+  let field;
+
+  if (type === EditType.Input) {
+    field = <Input />;
+  } else if (type === EditType.Select) {
+    field = (
+      <Select showSearch>
+        {options.map(({ name }) => (
+          <SelectOption key={name} value={name}>
+            {name}
+          </SelectOption>
+        ))}
+      </Select>
+    );
+  } else if (type === EditType.DatePicker) {
+    field = <DatePicker />;
+  } else if (type === EditType.DateRangePicker) {
+    field = <DateRangePicker />;
+  } else if (type === EditType.TimePicker) {
+    field = <TimePicker use12Hours format={TIME_FORMAT} />;
+  } else if (type === EditType.TimeRangePicker) {
+    field = <TimeRangePicker use12Hours format={TIME_FORMAT} />;
+  } else {
+    field = <TextArea rows={3} />;
+  }
+
+  return (
+    <Form.Item name={name} label={name} rules={[{ required, message: `Please select a ${name}.` }]}>
+      {field}
+    </Form.Item>
+  );
+};
 
 export default TripSectionModalField;
--- components/TypesOverview/atoms/TimeRangePickerType.tsx
+++ components/TypesOverview/atoms/TimeRangePickerType.tsx
@@ -2,9 +2,11 @@ import { FC } from 'react';
 import { Form, TimePicker } from 'antd';
 const { RangePicker: TimeRangePicker } = TimePicker;
 
+import { TIME_FORMAT } from 'constants/dateTimeFormats';
+
 const TimeRangePickerType: FC = () => (
   <Form.Item label="Time Range Picker">
-    <TimeRangePicker use12Hours />
+    <TimeRangePicker use12Hours format={TIME_FORMAT} />
   </Form.Item>
 );
 
